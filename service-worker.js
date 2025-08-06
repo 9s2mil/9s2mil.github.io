87ccb009ece1083a8f8f24f0f216bf488a121439
9s2mil <124743931+9s2mil@users.noreply.github.com>
2025-08-06 11:16:34 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,29 @@
+const CACHE_NAME = 'pwa-cache-' + Date.now(); 
+const FILES_TO_CACHE = [
+  './pwa.html',
+  './Study1.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('pwa-cache-v1').then(cache => {
-      return cache.addAll([
-        './Study1.html',
-        './pwa.html',
-        './manifest.json',
-        './icons/icon-192.png',
-        './icons/icon-512.png'
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    self.clients.claim().then(() => {
-      return self.clients.matchAll({ type: 'window' }).then(clients => {
-        clients.forEach(client => client.navigate(client.url));
-      });
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
   );
+
+  self.clients.matchAll({ type: 'window' }).then(clients => {
+    clients.forEach(client => client.navigate(client.url));
+  });
 });
 
 self.addEventListener('fetch', event => {
